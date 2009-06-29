55fe05d7312c8ded737fa21a8445132e7f4b6b74
digitalfredy <digitalfredy@39447e76-02d8-11de-802b-177316e4ac06>
2009-06-29 14:15:02 +0000

    eliminada carpeta conf (ejemplos de conf de svn)

--- talleres/CampusPartyColombia2009/fa_tb.v
+++ talleres/CampusPartyColombia2009/fa_tb.v
@@ -8,12 +8,12 @@ module TestBench;
 		Input = 000;
 
 
-		#1 Input[1] <= 1;
-		   Input[2] <= 0;
-		   Input[0] <= 0;
+		#1 Input <= 101;
 	        #1 Input[1] <= 0;
-		   Input[2] <= 0;
-		   Input[0] <= 1;
+		   Input[2] <= 1;
+		   Input[0] <= 0;
+	   #1 Input <= 000;
+	   
 	end
 
 	initial begin
