154d600e5a38633655ebd8a5fdc6f32ee0344f1b
digitalfredy <digitalfredy@39447e76-02d8-11de-802b-177316e4ac06>
2009-09-30 14:37:36 +0000

    Agregado Lab2

--- trunk/ld2ud/code/Laboratorio2_Lab.v
+++ trunk/ld2ud/code/Laboratorio2_Lab.v
@@ -1,11 +1,15 @@
 module Laboratorio2_Lab (/*AUTOARG*/ ) ;
    reg   [6:0] Combinacion2Digitos;
-
+   reg [5:0]   Estimulo;
+ 	       
    MayorQue63 U1 (RtaMayorQue63,Combinacion2Digitos);
+   
+   CombinatoriaCodigo U2 (RtaCombinatoriaCodigo,Estimulo[5:0]);
    
    
    initial begin
       Combinacion2Digitos = 7'b0000000;
+      Estimulo = 6'b000000;
       
    end
 
@@ -14,8 +18,17 @@ module Laboratorio2_Lab (/*AUTOARG*/ ) ;
       repeat (100) begin
 	 #1 Combinacion2Digitos <= Combinacion2Digitos + 1;
 	 
-      end 
+      end
    end
+
+   initial begin
+      repeat (62) begin
+	 #1 Estimulo <= Estimulo + 1;
+	 
+      end
+
+   end
+   
 
    initial begin
       $dumpfile ("Laboratorio2_Lab.vcd");
