ea901972f9789abea372dcb8b6b99a99f7b0b06b
digitalfredy <digitalfredy@39447e76-02d8-11de-802b-177316e4ac06>
2009-11-24 04:51:09 +0000

    Agregados Registro 4 bits y Banco de registros con su laboratorios de prueba

---
+++ ld2ud/code/ciscud/Banco8Registros16Bit_Lab.v
@@ -0,0 +1,51 @@
+module Banco8Registros16Bit_Lab (/*AUTOARG*/
+   // Outputs
+   Tupla, Reloj, Habilitar, DireccionEscritura, DireccionA,
+   DireccionB
+   ) ;
+   output reg [15:0] Tupla;
+   output reg Reloj, Habilitar;
+   output reg [2:0] DireccionEscritura, DireccionA, DireccionB;
+
+   wire   [15:0] RtaA,RtaB;
+
+   Banco8Registros16Bit 
+     BancoPrueba (RtaA,RtaB,Tupla,Reloj,Habilitar,DireccionEscritura,DireccionA,DireccionB);
+   initial begin
+      Tupla     = 16'h0;
+      Reloj     = 1'b0;
+      Habilitar = 1'b1;
+      DireccionA = 3'b111;
+      DireccionB = 3'b100;
+   end
+
+   initial begin
+      repeat (24) begin
+	 #1 Reloj <= ~Reloj;
+      end
+   end
+   
+   initial begin
+      repeat (16) begin
+	 #1 Tupla <= Tupla+1;
+      end
+   end
+
+   initial begin
+      #3 DireccionEscritura = 3'b111;
+   end
+
+   initial begin
+      #5 DireccionEscritura = 3'b100;
+   end
+   
+   initial begin
+      #9 Habilitar = 1'b0;      
+   end
+
+   initial begin
+      $dumpfile ("Banco8Registros16Bit_Lab.vcd");
+      $dumpvars;
+   end  
+   
+endmodule // Banco8Registros16Bit_Lab
---
+++ ld2ud/code/ciscud/Registro4Bits_Lab.v
@@ -0,0 +1,41 @@
+module Registro14Bits_Lab (/*AUTOARG*/
+   // Outputs
+   Tupla, Reloj, Reiniciar, Habilitar
+   ) ;
+   output reg [3:0] Tupla;
+   output reg Reloj, Reiniciar, Habilitar;
+
+   wire   [3:0] RtaRegistro;
+   
+   Registro4Bit RegistroPrueba (RtaRegistro, Tupla, Reloj, Reiniciar, Habilitar);
+
+   //Simulación:
+   initial begin
+      Tupla     = 4'b0000;
+      Reloj     = 1'b0;
+      Reiniciar = 1'b0;
+      Habilitar = 1'b0;
+   end
+
+   initial begin
+      repeat (8) begin
+	 #1 Reloj <= ~Reloj;
+      end
+   end
+
+   initial begin
+      repeat (8) begin
+	 #1 Tupla <= Tupla+1;
+      end
+   end
+   
+   initial begin
+      #3 Habilitar = 1'b1;
+   end
+
+   initial begin
+      $dumpfile ("Registro4Bits_Lab.vcd");
+      $dumpvars;
+   end  
+   
+endmodule // Registro4Bits_Lab
