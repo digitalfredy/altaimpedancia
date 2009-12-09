d24ad108e520cff192eef3f185405837fbd288f0
guevara.ernesto <guevara.ernesto@39447e76-02d8-11de-802b-177316e4ac06>
2009-12-09 10:58:55 +0000

    Añadido proyecto en ise

--- ld2ud/code/ciscud/compilar.sh
+++ ld2ud/code/ciscud/compilar.sh
@@ -1,1 +1,1 @@
-iverilog CiscUd.v UnidadDeControl.v Multiplexor16Bits2X1.v Banco8Registros16Bit.v Alsu16Bits.v Registro16Bits.v Buffer3Estados16Bit.v SumaUno.v Cero.v Multiplexor16Bits4X1.v Registro6Bits.v
+iverilog CiscUd.v UnidadDeControl.v Multiplexor16Bits2X1.v Banco8Registros16Bit.v Alsu16Bits.v Registro16Bits.v Buffer3Estados16Bit.v SumaUno.v Cero.v Multiplexor16Bits4X1.v Registro6Bits.v Memoria16X16Bits.v
