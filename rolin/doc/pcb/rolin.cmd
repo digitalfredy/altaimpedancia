de13510d7ef57870a5393aa2ac1d905c2123aa49
digitalfredy <digitalfredy@39447e76-02d8-11de-802b-177316e4ac06>
2009-12-10 18:26:07 +0000

    Moviendo a doc lo que es de doc

---
+++ rolin/doc/pcb/rolin.cmd
@@ -0,0 +1,125 @@
+# Pin name action command file
+
+# Start of element D8
+ChangePinName(D8, 1, 1)
+ChangePinName(D8, 2, 2)
+
+# Start of element D7
+ChangePinName(D7, 1, 1)
+ChangePinName(D7, 2, 2)
+
+# Start of element D2
+ChangePinName(D2, 1, 1)
+ChangePinName(D2, 2, 2)
+
+# Start of element C1
+ChangePinName(C1, 2, 2)
+ChangePinName(C1, 1, 1)
+
+# Start of element D4
+ChangePinName(D4, 1, 1)
+ChangePinName(D4, 2, 2)
+
+# Start of element D3
+ChangePinName(D3, 1, 1)
+ChangePinName(D3, 2, 2)
+
+# Start of element CONN2
+ChangePinName(CONN2, 1, 1)
+ChangePinName(CONN2, 2, 2)
+
+# Start of element CONN1
+ChangePinName(CONN1, 9, 9)
+ChangePinName(CONN1, 10, 10)
+ChangePinName(CONN1, 7, 7)
+ChangePinName(CONN1, 5, 5)
+ChangePinName(CONN1, 3, 3)
+ChangePinName(CONN1, 1, 1)
+ChangePinName(CONN1, 8, 8)
+ChangePinName(CONN1, 6, 6)
+ChangePinName(CONN1, 4, 4)
+ChangePinName(CONN1, 2, 2)
+
+# Start of element U3
+ChangePinName(U3, 22, GND)
+ChangePinName(U3, 8, GND)
+ChangePinName(U3, 7, VCC)
+ChangePinName(U3, 28, (SCL) PC5)
+ChangePinName(U3, 27, (SDA) PC4)
+ChangePinName(U3, 26, (ADC3) PC3)
+ChangePinName(U3, 25, (ADC2) PC2)
+ChangePinName(U3, 24, (ADC1) PC1)
+ChangePinName(U3, 23, (ADC0) PC0)
+ChangePinName(U3, 21, AREF)
+ChangePinName(U3, 20, AVCC)
+ChangePinName(U3, 19, (SCK) PB5)
+ChangePinName(U3, 18, (MISO) PB4)
+ChangePinName(U3, 17, (MOSI) PB3)
+ChangePinName(U3, 16, (OC1B) PB2)
+ChangePinName(U3, 15, (OC1A) PB1)
+ChangePinName(U3, 14, PB0 (ICP1))
+ChangePinName(U3, 13, PD7 (AIN1))
+ChangePinName(U3, 12, PD6 (AIN0))
+ChangePinName(U3, 11, PD5 (T1))
+ChangePinName(U3, 10, PB7 (TOSC1))
+ChangePinName(U3, 9, PB6 (TOSC1))
+ChangePinName(U3, 6, PD4 (T0))
+ChangePinName(U3, 5, PD3 (INT1))
+ChangePinName(U3, 4, PD2 (INT0))
+ChangePinName(U3, 3, PD1 (TXD))
+ChangePinName(U3, 2, PD0 (RXD))
+ChangePinName(U3, 1, PC6 (RST))
+
+# Start of element CONN4
+ChangePinName(CONN4, 1, 1)
+ChangePinName(CONN4, 2, 2)
+
+# Start of element CONN3
+ChangePinName(CONN3, 5, 5)
+ChangePinName(CONN3, 3, 3)
+ChangePinName(CONN3, 1, 1)
+ChangePinName(CONN3, 6, 6)
+ChangePinName(CONN3, 4, 4)
+ChangePinName(CONN3, 2, 2)
+
+# Start of element C3
+ChangePinName(C3, 2, 2)
+ChangePinName(C3, 1, 1)
+
+# Start of element C2
+ChangePinName(C2, 2, 2)
+ChangePinName(C2, 1, 1)
+
+# Start of element XTAL1
+ChangePinName(XTAL1, 2, 2)
+ChangePinName(XTAL1, 1, 1)
+
+# Start of element D1
+ChangePinName(D1, 1, 1)
+ChangePinName(D1, 2, 2)
+
+# Start of element D6
+ChangePinName(D6, 1, 1)
+ChangePinName(D6, 2, 2)
+
+# Start of element U1
+ChangePinName(U1, 9, ENABLE 2)
+ChangePinName(U1, 10, INPUT 3)
+ChangePinName(U1, 11, OUTPUT 3)
+ChangePinName(U1, 12, GND)
+ChangePinName(U1, 13, GND)
+ChangePinName(U1, 14, OUTPUT 4)
+ChangePinName(U1, 15, INPUT 4)
+ChangePinName(U1, 16, Vss)
+ChangePinName(U1, 8, Vs)
+ChangePinName(U1, 7, INPUT 2)
+ChangePinName(U1, 6, OUTPUT 2)
+ChangePinName(U1, 5, GND)
+ChangePinName(U1, 4, GND)
+ChangePinName(U1, 3, OUTPUT 1)
+ChangePinName(U1, 2, INPUT 1)
+ChangePinName(U1, 1, ENABLE 1)
+
+# Start of element D5
+ChangePinName(D5, 1, 1)
+ChangePinName(D5, 2, 2)
